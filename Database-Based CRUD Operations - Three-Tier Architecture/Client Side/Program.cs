9d06f62cbe735f4e2937baada8cef3f6a012f6aa
Ömer MEMES <omer.memes20@gmail.com>
2024-10-03 10:57:25 +0300

    Get All Employee - Client Side Server

---
+++ Database-Based CRUD Operations - Three-Tier Architecture/Client Side/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Net.Http.Json;
+using System.Threading.Channels;
+
+namespace Client_Side
+{
+    public class Program
+    {
+        public static readonly HttpClient httpClient = new HttpClient();
+        public static async Task Main(string[] args)
+        {
+            httpClient.BaseAddress = new Uri("https://localhost:7074/api/Employees/");
+
+            await GetAllEmployees();
+        }
+
+        static async Task GetAllEmployees()
+        {
+            try
+            {
+                var employees = await httpClient.GetFromJsonAsync<List<clsEmployee>>("GetAllEmployees");
+
+                if (employees == null)
+                {
+                    Console.WriteLine("BadRequest: Data was not fetched successfully.");
+                    return;
+                }
+
+                if (employees.Count == 0)
+                {
+                    Console.WriteLine("NotFound: No Data To List");
+                }
+
+                employees.Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Database-Based CRUD Operations - Three-Tier Architecture/Client Side/clsEmployeeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client_Side
+{
+    public static class clsEmployeeExtension
+    {
+        public static void Print(this List<clsEmployee> source)
+        {
+            source.ForEach(e => Console.WriteLine(e));
+        }
+    }
+}
