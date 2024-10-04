0fe223ce89cfa0b66728d15d666712de9ce2dc94
Ömer MEMES <omer.memes20@gmail.com>
2024-10-04 07:10:17 +0300

    Add New Employee - Client Side Code

--- Database-Based CRUD Operations (T-SQL and Three-Tier Architecture)/Client Side/Program.cs
+++ Database-Based CRUD Operations (T-SQL and Three-Tier Architecture)/Client Side/Program.cs
@@ -6,6 +6,26 @@ namespace Client_Side
     {
         public static readonly HttpClient httpClient = new HttpClient();
 
+        private static clsEmployee employee = new clsEmployee()
+        {
+            FirstName = "Ali",
+            LastName = "Sultan",
+            Age = 22,
+            Salary = 24000m,
+            HireDate = DateTime.Now,
+            TerminationDate = null,
+        };
+
+        private static clsEmployee invalidEmployeeData = new clsEmployee()
+        {
+            FirstName = "Al",
+            LastName = "s",
+            Age = 80,
+            Salary = -24000m,
+            HireDate = DateTime.Now,
+            TerminationDate = null,
+        };
+
         public static async Task Main(string[] args)
         {
             httpClient.BaseAddress = new Uri("https://localhost:7026/api/Employees/");
@@ -15,6 +35,9 @@ namespace Client_Side
             await GetEmployeeByID(1);
             await GetEmployeeByID(-1);
             await GetEmployeeByID(100);
+
+            await AddNewEmployee(employee);
+            await AddNewEmployee(invalidEmployeeData);
         }
 
         static async Task GetAllEmployees()
@@ -76,6 +99,40 @@ namespace Client_Side
                 else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
                 {
                     Console.WriteLine($"Bad Request: Not Accepted ID{{{id}}}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static async Task AddNewEmployee(clsEmployee employee)
+        {
+            try
+            {
+                if (!clsEmployeeValidator.Validator(false).Validate(employee).IsValid)
+                {
+                    Console.WriteLine("BadRequest: Invalid Employee Data");
+                    return;
+                }
+
+                var response = await httpClient.PostAsJsonAsync<clsEmployee>("AddNewEmployee", employee);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    clsEmployee? insertedEmployee = await response.Content.ReadFromJsonAsync<clsEmployee>();
+
+                    if (insertedEmployee is not null)
+                    {
+                        Console.WriteLine(insertedEmployee);
+                    }
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    Console.WriteLine("BadRequest: Invalid Employee Data");
+                    return;
                 }
             }
             catch (Exception ex)
